ac3af53448e30a47a4bf427ae6db99c433982582
Aline <likasemog@gmail.com>
2023-10-11 13:50:14 -0300

    desafio
    Signed-off-by: Aline <likasemog@gmail.com>

---
+++ src/parametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class parametrosInvalidosException extends Exception {
+    public parametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro.");
+    }
+}
